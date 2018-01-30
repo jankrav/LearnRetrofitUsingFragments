8f5a2dec25f5a23caaf3190f548bbdebf6322d65
Ivan Kravchuk <ivankravchuk10@gmail.com>
2018-01-30 14:18:59 +0200

    created class which will handle request to the server

---
+++ app/src/main/java/com/jankrav/learnretrofitusingfragmens/model/client/GitHubClient.java
@@ -0,0 +1,7 @@
+package com.jankrav.learnretrofitusingfragmens.model.client;
+
+
+public class GitHubClient {
+    private static final GitHubService service = ServiceGenerator.getDefaultService();
+
+}
