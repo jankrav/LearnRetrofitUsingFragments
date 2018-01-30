1a03e856cda3035e936d08a8be160304f501ae48
Ivan Kravchuk <ivankravchuk10@gmail.com>
2018-01-31 00:04:55 +0200

    created two test class for testing presenters

---
+++ app/src/androidTest/java/com/jankrav/learnretrofitusingfragmens/TestChoosePresenter.java
@@ -0,0 +1,14 @@
+package com.jankrav.learnretrofitusingfragmens;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class TestChoosePresenter {
+    @Test
+    public void makeSmth() throws Exception{
+
+    }
+}
---
+++ app/src/androidTest/java/com/jankrav/learnretrofitusingfragmens/TestDetailPresenter.java
@@ -0,0 +1,5 @@
+package com.jankrav.learnretrofitusingfragmens;
+
+
+public class TestDetailPresenter {
+}
